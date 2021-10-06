2229dde2c5ca41637776d7161ad3016851e38e4d
Subhamdas20 <subham21.sd@gmail.com>
2021-10-06 23:52:22 +0530

    Created a BST and searched the element and verified using test cases

--- src/main/java/com/bz/binarysearchtree/MyBinaryTree.java
+++ src/main/java/com/bz/binarysearchtree/MyBinaryTree.java
@@ -27,4 +27,26 @@ public class MyBinaryTree <T extends Comparable<T>> {
     private int getSizeRecursive(MyBinaryNode<T> current) {
         return current == null ? 0 :1 + this.getSizeRecursive(current.left) + this.getSizeRecursive(current.right);
     }
+//    public boolean search(T key){
+//        boolean result = false;
+//        if (root.key == key){
+//             result =true;
+//        }
+//        return result;
+//    }
+   public boolean search(T key)  {
+    root = search_Recursive(root, key);
+    if (root!= null)
+        return true;
+    else
+        return false;
+}
+    public MyBinaryNode search_Recursive(MyBinaryNode root, T key)  {
+        if (root==null || root.key==key)
+            return root;
+        if (root.key.compareTo(key) >0)
+            return search_Recursive(root.left, key);
+        else
+        return search_Recursive(root.right, key);
+    }
 }
